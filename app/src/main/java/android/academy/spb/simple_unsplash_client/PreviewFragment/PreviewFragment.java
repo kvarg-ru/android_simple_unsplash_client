56c82a5ca9b17952c8bbfcf97f45243276adc8e8
Mikhail Basov <mixa@multi.fishgate.ru>
2018-05-31 12:34:43 +0300

    Add inside application browser tabs based on ChromeTabs

--- app/src/main/java/android/academy/spb/simple_unsplash_client/PreviewFragment/PreviewFragment.java
+++ app/src/main/java/android/academy/spb/simple_unsplash_client/PreviewFragment/PreviewFragment.java
@@ -8,7 +8,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -80,10 +82,11 @@ public class PreviewFragment extends Fragment {
     private void openBrowser(String url) {
 
         Uri webpage = Uri.parse(url);
-        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivity(intent);
-        }
+
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(getContext(), webpage);
 
     }
 
